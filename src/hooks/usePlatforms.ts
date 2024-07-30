ea451061ee1499f4f5519a69cbd6fa5f8b4518e7
hassanjavaid07 <scuderia.hj@gmail.com>
2024-07-30 20:27:56 +0500

    Remove duplicate platform interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,13 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import apiClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "./useGames";
 
-// interface Platform {
-//   id: number;
-//   name: string;
-//   slug: string;
-// }
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 const usePlatforms = () =>
   useQuery({
