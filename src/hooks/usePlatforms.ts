f3b7d2e8eb3c77bcdead22cfd85fe2a3934281ce
hassanjavaid07 <scuderia.hj@gmail.com>
2024-07-30 20:04:44 +0500

    Refactoring - add react query object to fetch games, genres, platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient, { FetchResponse } from "../services/api-client";
+import { Platform } from "./useGames";
 
 // interface Platform {
 //   id: number;
@@ -6,6 +9,15 @@ import platforms from "../data/platforms";
 //   slug: string;
 // }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["/platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/paresnts")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
