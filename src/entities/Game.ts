66f39a7a354236c39b0dcfb224ce73dd18f7aae6
hassanjavaid07 <scuderia.hj@gmail.com>
2024-08-04 12:56:57 +0500

    Buıld Game Attrıbutes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,12 +1,16 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
   rating_top: number;
   description_raw: string;
+  publishers: Publisher[];
 }
