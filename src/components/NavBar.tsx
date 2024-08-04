b3e20307659d4ee420849038c70bdb1530f3f919
hassanjavaid07 <scuderia.hj@gmail.com>
2024-08-04 15:15:56 +0500

    Adjust game logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <HStack padding="10px">
       <Link to="/">
-        <Image src={logo} boxSize="60px" />
+        <Image src={logo} boxSize="80px" />
       </Link>
       <SearchInput />
       <ColorModeSwitch />
