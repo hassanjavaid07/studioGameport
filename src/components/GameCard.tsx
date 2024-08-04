f26e6951854afbf60b6182c434a3fb5e8c11a04c
hassanjavaid07 <scuderia.hj@gmail.com>
2024-08-04 15:23:39 +0500

    Refactor entities

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,7 +4,7 @@ import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import Emoji from "./Emoji";
 import { Link } from "react-router-dom";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 
 interface Props {
   game: Game;
